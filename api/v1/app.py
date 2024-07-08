7951687f6efaae8f164c45f3fd29628218acba9a
Gisezegk <anatanonamewanamedesuka2@gail.com>
2024-07-08 23:16:21 +0000

    App

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,12 @@
-#!/usr/bin/python3
-"""application"""
-from flask import Flask, make_response, jsonify
-from models import storage
+# api/v1/app.py
+
+from flask import Flask
 from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
 
-"""instancies my app"""
 app = Flask(__name__)
-"""register blueprint template in my appi"""
+
+# Register the Blueprint
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-
-@app.teardown_appcontext
-def close_storage(exception):
-    """Close the storage"""
-    storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """handle 404 error"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-if __name__ == '__main__':
-    """run my app"""
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
